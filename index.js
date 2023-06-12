ef97d1aa5ff8d61f62070fd2afda3e2d6a389aa3
Drew Jordan <ajordan12@luc.edu>
2023-06-12 17:26:54 -0500

    outline

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // housekeeping 
+const inquirer = require('inquirer');
+const fs = require('fs');
+//define baase shape class with common properties and methods
 
-// 
+// define subclasses for each type of shape that extend the shape class
+
+// prompt the user for the shape they want to create
+    // text 3 characters long
+    // text color
+    // background color
+    // border color
+    // shape
+
+// generate the design based on the user input
+
+// save the design to a file
