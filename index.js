72cec31a5bb7e0fa966eb83fcddddb94d6026cd6
Drew Jordan <ajordan12@luc.edu>
2023-06-12 17:35:17 -0500

    questions added

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 // housekeeping 
 const inquirer = require('inquirer');
 const fs = require('fs');
-//define baase shape class with common properties and methods
-
-// define subclasses for each type of shape that extend the shape class
+const questions = [
+    {
+        type: 'list',
+        message: 'What shape do you want to create?',
+        choices: ['square', 'circle', 'triangle'],
+        name: 'shape',
+    },
+    {
+        type: 'input',
+        message: 'What is the text you want to display?',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'What color do you want the text to be?',
+        name: 'textColor',
+    },
+    {
+        type: 'input',
+        message: 'What color do you want the background to be?',
+        name: 'backgroundColor',
+    },
+    {
+        type: 'input',
+        message: 'What color do you want the border to be?',
+        name: 'borderColor',
+    },
+]
 
 // prompt the user for the shape they want to create
-    // text 3 characters long
-    // text color
-    // background color
-    // border color
-    // shape
-
+inquirer.createPromptModule()(questions).then((answers) => {
+    console.log(answers);
+});
+//define baase shape class with common properties and methods
+// class Shape {
+//     constructor(name, text, textColor, backgroundColor, Shape) {
+//         this.name = name;
+//         this.text = text;
+//         this.textColor = textColor;
+//         this.backgroundColor = backgroundColor;
+//         this.Shape = Shape;
+//     }
+// }
+// define subclasses for each type of shape that extend the shape class
 // generate the design based on the user input
 
 // save the design to a file
